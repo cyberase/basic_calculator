b33ec308aa0db91b00a8ff380f3f383979aeb48e
Nurdaulet <goghgary@gmail.com>
2022-02-04 03:42:20 +0200

    basic_calculator_using_my_name
    I used my name to create the calculator

---
+++ basic_calculator_using_my_name.py
@@ -0,0 +1,84 @@
+#Basic Calculator
+#Enter first digit then second the operator
+print ('This a super basic capculator\n')
+print ('Created by BroFarOps 4/27/2019\n\n')
+nur = float(input("Enter The First Number\n" ))
+print(nur)
+dau = float(input("Enter the Second number\n"))
+print(dau)
+let = input("Enter the operator\n")
+
+if let =="+":
+    print ('Addition')
+elif let =="-":
+    print ('Subtraction')
+elif let =="*":
+    print ('Multiplication')
+elif let =="x":
+    print ('Multiplication')
+elif let =="/":
+    print ('Division')
+elif let =="%":
+    print ('Percentage')
+elif let =="a":
+    print ('add')
+elif let =="s":
+    print ('sub')
+elif let =="m":
+    print ('mul')
+elif let =="d":
+    print ('div')
+elif let =="p":
+    print ('per')
+elif let =="A":
+    print ('add')
+elif let =="S":
+    print ('sub')
+elif let =="M":
+    print ('mul')
+elif let =="D":
+    print ('div')
+elif let =="P":
+    print ('per')
+
+print("\nYour Expression and Answer is:" )
+print(nur)
+print(dau)
+print (let)
+print("--------\n")
+
+if let =="+":
+    print (nur+dau)
+elif let =="-":
+    print (nur-dau)
+elif let =="*":
+    print (nur*dau)
+elif let =="x":
+    print (nur*dau)
+elif let =="/":
+    print (nur/dau)
+elif let =="%":
+    print (nur%dau)
+elif let =="a":
+    print (nur+dau)
+elif let =="s":
+    print (nur-dau)
+elif let =="m":
+    print (nur*dau)
+elif let =="d":
+    print (nur/dau)
+elif let =="p":
+    print (nur%dau)
+elif let =="A":
+    print (nur+dau)
+elif let =="S":
+    print (nur-dau)
+elif let =="M":
+    print (nur*dau)
+elif let =="D":
+    print (nur/dau)
+elif let =="P":
+    print (nur%dau)
+else:
+    print ("INVALID INPUT\n")
+    print ('D D O')
